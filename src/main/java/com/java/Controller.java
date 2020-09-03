2aa27b004a78f4d19693302ddca5c835271d1922
Rajiv Ranjan <rajivranjanmca07@gmail.com>
2020-09-03 06:45:30 +0530

    second

---
+++ src/main/java/com/java/Controller.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/org")
+public class Controller {
+
+	@GetMapping
+	public String getMsg() {
+		return "Hello Rajiv welcome to git";
+	}
+}
--- src/main/java/com/java/Test.java
+++ src/main/java/com/java/Test.java
@@ -1,10 +1,15 @@
 package com.java;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SpringApplication.run(Test.class, args);
+		System.out.println("Server started .....");
 	}
 
 }
